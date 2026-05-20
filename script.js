ad9daabced072556982d87e1169783765f2d0a1f
Victor Matheus M. Vieira <huedits@users.noreply.github.com>
2026-05-20 18:09:08 -0300

    correcao do parent da twitch

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function getEmbedUrl(platform, channelOrId) {
                 twitchChannel = twitchMatch[1];
             }
             return {
-                url: `https://player.twitch.tv/?channel=${encodeURIComponent(twitchChannel)}&parent=huedits.github.io`,
+                url: `https://player.twitch.tv/?channel=${encodeURIComponent(twitchChannel)}&parent=huedits.github.io/streamviewer`,
                 channel: twitchChannel
             };
         
