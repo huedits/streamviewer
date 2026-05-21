89e316e530b6bb1c01718c57bc887adebf12861b
Victor Matheus M. Vieira <huedits@users.noreply.github.com>
2026-05-20 21:45:21 -0300

    thiner header

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration and Constants
 const CONFIG = {
     // Application version
-    version: '2.1.4',
+    version: '2.1.5',
     
     // Twitch embed parent domain - hardcoded for GitHub Pages
     twitchParent: 'huedits.github.io',
