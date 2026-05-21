5c9787e713ab83ccf70a7fc27f817c3ef96de20b
Victor Matheus M. Vieira <huedits@users.noreply.github.com>
2026-05-20 23:13:40 -0300

    svg fix

--- js/config.js
+++ js/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
         kick: {
             name: 'Kick',
             color: '#53fc18',
-            icon: `<svg class="platform-icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2">
-            <path d="M37 .036h164.448v113.621h54.71v-56.82h54.731V.036h164.448v170.777h-54.73v56.82h-54.711v56.8h54.71v56.82h54.73V512.03H310.89v-56.82h-54.73v-56.8h-54.711v113.62H37V.036z" fill="#53fc18"/>
+            icon: `<svg class="platform-icon" viewBox="0 0 24 24" fill="#53fc18">
+                <path d="M37 .036h164.448v113.621h54.71v-56.82h54.731V.036h164.448v170.777h-54.73v56.82h-54.711v56.8h54.71v56.82h54.73V512.03H310.89v-56.82h-54.73v-56.8h-54.711v113.62H37V.036z"/>
             </svg>`
         },
         youtube: {
