2279a577d4db4ee2bd6a2a35e876bbb63498f889
Victor Matheus M. Vieira <huedits@users.noreply.github.com>
2026-05-21 02:54:24 -0300

    single card fix

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // Configuration and Constants
 const CONFIG = {
     // Application version
-    version: '2.2.3',
+    version: '2.2.4',
     
     // Twitch embed parent domain - hardcoded for GitHub Pages
     twitchParent: 'huedits.github.io',
